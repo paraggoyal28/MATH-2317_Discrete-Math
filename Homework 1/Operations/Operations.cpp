330ddde97083fe2d69cfe3e10118339cbc2df3bf
BedirT <bedir.tapkan.53@gmail.com>
2016-09-26 15:35:19 -0500

    Missippi updated

--- Homework 1/Operations/Operations.cpp
+++ Homework 1/Operations/Operations.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #define pb push_back
 using namespace std;
-string op3(string a, int b){
+string op4(string a, int b){
     string s = "";
     if(b > 5)
         s += a + "^" + a + "^" + a + ".....(" + to_string(b) + " times)" + ".....^" + a;
@@ -15,32 +15,32 @@ string op3(string a, int b){
     return s;
 }
 
-string op4(string a, int b){
-    string s = "";
-    if(b > 5)
-        s += op3(a, stoi(a)) + "\n------\n...\n(" + to_string(b) + " times)\n...\n------\n" + op3(a, stoi(a));
-    else{
-        for(int i = 0; i < b-1; ++i)
-            s += op3(a, stoi(a)) + "\n---------\n";
-        s += op3(a, stoi(a));
-    }
-    return s;
-}
-
 string op5(string a, int b){
     string s = "";
     if(b > 5)
-        s += op4(a, stoi(a)) + "\n******************************************\n.\n.\n.\n(" + to_string(b) + " times)\n.\n.\n.\n******************************************\n" + op4(a, stoi(a));
+        s += op4(a, stoi(a)) + "\n------\n...\n(" + to_string(b) + " times)\n...\n------\n" + op4(a, stoi(a));
     else{
         for(int i = 0; i < b-1; ++i)
-            s += op4(a, stoi(a)) + "\n******************************************\n";
+            s += op4(a, stoi(a)) + "\n---------\n";
         s += op4(a, stoi(a));
     }
     return s;
 }
 
+string op6(string a, int b){
+    string s = "";
+    if(b > 5)
+        s += op5(a, stoi(a)) + "\n******************************************\n.\n.\n.\n(" + to_string(b) + " times)\n.\n.\n.\n******************************************\n" + op5(a, stoi(a));
+    else{
+        for(int i = 0; i < b-1; ++i)
+            s += op5(a, stoi(a)) + "\n******************************************\n";
+        s += op5(a, stoi(a));
+    }
+    return s;
+}
+
 int main (){
-    int b, c; // 5 op5 4 op5 3
+    int b, c; // 5 op6 4 op6 3
     vector<string> svc;
     string s = "";
     while(s != "="){
@@ -50,9 +50,9 @@ int main (){
     string str;
     str = svc[0];
     for (int it = 1; it < svc.size() ; it+=2) {
-        if (svc[it] == "op3"){ str = op3(str , stoi(svc[it+1])); }
-        else if (svc[it] == "op4"){ str = op4(str, stoi(svc[it+1])); }
+        if (svc[it] == "op4"){ str = op4(str , stoi(svc[it+1])); }
         else if (svc[it] == "op5"){ str = op5(str, stoi(svc[it+1])); }
+        else if (svc[it] == "op6"){ str = op6(str, stoi(svc[it+1])); }
         else;
     }
     cout << str << endl;
