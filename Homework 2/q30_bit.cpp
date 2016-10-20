f2ce8afe5eafefae2a29ff2b5613ae066233e110
BedirT <bedir.tapkan.53@gmail.com>
2016-10-20 06:36:19 -0500

    q30 library added

--- Homework 2/q30_bit.cpp
+++ Homework 2/q30_bit.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <stdlib.h>
 using namespace std;
+int n;
 
 void bit(int x){
     for(int i = 0; i < n; ++i)
@@ -11,11 +12,10 @@ void bit(int x){
 
 int main (){
 
-    int n, size;
     cout << "Please enter the number of elements.\n";
     cin >> n;
 
-    for(int i = 0; i < power(2,n); ++i){
+    for(int i = 0; i < (1 << n); ++i){
         bit(i);
         cout << endl;
     }
